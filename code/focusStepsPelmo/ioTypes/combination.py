8cb68b1fb6081fba7d2b9e2fdb9a3875b3283e06
Ben Konrad Meyer <Ben.Meyer@bayer.com>
2024-06-25 14:53:17 +0200

    Type fixing dataclass

--- code/focusStepsPelmo/ioTypes/combination.py
+++ code/focusStepsPelmo/ioTypes/combination.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+from util.datastructures import TypeCorrecting
 from util.conversions import map_to_class
 
 from .compound import Compound
@@ -7,13 +8,14 @@ from .gap import GAP
 
 
 @dataclass(frozen = True)
-class Combination:
+class Combination(TypeCorrecting):
+    """A dataclass combining a gap and a compound definition"""
     gap: GAP
     compound: Compound
 
-    def __post_init__(self):
-        object.__setattr__(self, 'gap', map_to_class(self.gap, GAP))
-        object.__setattr__(self, 'compound', map_to_class(self.compound, Compound))
+    # def __post_init__(self):
+    #     object.__setattr__(self, 'gap', map_to_class(self.gap, GAP))
+    #     object.__setattr__(self, 'compound', map_to_class(self.compound, Compound))
 
     def _asdict(self):
         return {'gap': self.gap._asdict(), 'compound': self.compound}
--- code/focusStepsPelmo/util/datastructures.py
+++ code/focusStepsPelmo/util/datastructures.py
@@ -1,7 +1,9 @@
 
 
 from collections import OrderedDict, UserDict
-from typing import Any, Dict, Optional
+from dataclasses import is_dataclass
+from typing import Any, Dict, Optional, Type, TypeVar
+import typing
 
 
 class RSDict(UserDict):
@@ -23,4 +25,28 @@ class HashableDict(UserDict):
 class HashableRSDict(RSDict):
     def __hash__(self):
         return hash(frozenset(self.items()))
+    
+class TypeCorrecting:
+    def __post_init__(self):
+        type_hints = typing.get_type_hints(self)
+        for attr, t in type_hints.items():
+            object.__setattr__(self, attr, correct_type(self.__getattribute__(attr), t))
+            
+
+T = TypeVar('T')
+def correct_type(input_value: Any, t: Type[T]) -> T:
+    if type(input_value) == t:
+        return input_value
+    if hasattr(t, '_name'):
+        if t._name == 'Tuple':
+            return tuple([correct_type(x, t.__args__[0]) for x in input_value])
+        if t._name == 'List':
+            return [correct_type(x, t.__args__[0]) for x in input_value]
+        if t._name == 'Dict':
+            return dict({correct_type(key, t.__args__[0]): correct_type(value, t.__args__[1]) for key, value in input_value.items()})
+    if is_dataclass(t):
+        return t(**input_value)
+    return t(input_value)
+
+
 
