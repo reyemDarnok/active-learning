a5ac324d657484ad0e9e6c1094b6644fed2e088e
Ben Konrad Meyer <Ben.Meyer@bayer.com>
2024-06-28 15:22:10 +0200

    fix tuples in type correcting

--- focusStepsPelmo/util/datastructures.py
+++ focusStepsPelmo/util/datastructures.py
@@ -1,9 +1,10 @@
+import itertools
 import sys
 import typing
 from collections import OrderedDict, UserDict
 from dataclasses import is_dataclass
 from enum import Enum
-from typing import Any, Dict, List, Optional, Tuple, Type, TypeVar, Union
+from typing import Any, Dict, List, Optional, Type, TypeVar, Union
 
 
 class RSDict(UserDict):
@@ -47,65 +48,73 @@ class TypeCorrecting:
 T = TypeVar('T')
 
 
+class NoValidStrategyException(Exception):
+    def __init__(self, *args, strategy_exceptions: List[Exception]):
+        super().__init__(*args)
+        self.strategy_exceptions = strategy_exceptions
+
+
 def correct_type(input_value: Any, t: Type[T]) -> T:
     if type(input_value) == t:
         return input_value
     if hasattr(t, '__origin__'):  # Typing with Type Vars
         origin = t.__origin__
         # noinspection PyProtectedMember
-        if origin == Union and type(None) in t.__args__:
-            if input_value:
-                return correct_type(input_value, t.__args__[0])
+        if origin == Union:
+            if type(None) in t.__args__ and input_value is None:
+                return None
             else:
-                t = t.__args__[0]
-                return correct_type(None, t)
-        elif origin == Tuple:
-            if not input_value:
-                return tuple()
-            type_args = t.__args__
-            if not isinstance(type_args[0], TypeVar):
-                return tuple([correct_type(x, type_args[0]) for x in input_value])
-            else:
-                return tuple(input_value)
+                exceptions = []
+                for union_type in t.__args__:
+                    try:
+                        return correct_type(input_value, union_type)
+                    except Exception as e:
+                        exceptions.append(e)
+                raise NoValidStrategyException("Could not find a type that works with the value",
+                                               strategy_exceptions=exceptions)
         elif origin == tuple:
-            if input_value:
-                return tuple(input_value)
-            else:
-                return tuple()
-        elif origin == List:
             if not input_value:
-                return list(input_value)
-            type_args = t.__args__
-            if not isinstance(type_args[0], TypeVar):
+                return tuple()
+            if hasattr(t, '__args__') and not isinstance(t.__args__[0], TypeVar):
+                type_args = t.__args__
+                result = tuple()
+                last_type = type_args[-2] if len(type_args) > 1 else type_args[1]
+                for value, value_type in itertools.zip_longest(input_value, type_args, fillvalue=last_type):
+                    if value_type != Ellipsis:
+                        result += (correct_type(value, value_type),)
+                    else:
+                        if value == last_type:
+                            break  # Ellipsis is not taken, i.e. a tuple of one or more ints has one int
+                        else:
+                            result += (correct_type(value, last_type),)
+                return result
+            else:
+                return tuple(input_value)
+        elif origin == list:
+            if not input_value:
+                return list()
+            if hasattr(t, '__args__') and not isinstance(t.__args__[0], TypeVar):
                 return [correct_type(x, t.__args__[0]) for x in input_value]
             else:
                 return list(input_value)
-        elif origin == list:
-            if input_value:
-                return list(input_value)
-            else:
-                return list()
-        elif origin == Dict:
+        elif origin == dict:
             if not input_value:
                 return {}
-            type_args = t.__args__
             keys = input_value.keys()
-            if not isinstance(type_args[0], TypeVar):
-                keys = (correct_type(key, type_args[0]) for key in keys)
-            if not isinstance(type_args[1], TypeVar):
-                return {key: correct_type(input_value[key], type_args[1]) for key in keys}
-            else:
-                return {key: input_value[key] for key in keys}
-        elif origin == dict:
-            if origin:
-                return dict(input_value)
-            else:
-                return dict()
+            if hasattr(t, '__args__'):
+                type_args = t.__args__
+                if not isinstance(type_args[0], TypeVar):
+                    keys = (correct_type(key, type_args[0]) for key in keys)
+                if not isinstance(type_args[1], TypeVar):
+                    return {key: correct_type(input_value[key], type_args[1]) for key in keys}
+            return {key: input_value[key] for key in keys}
         elif t._name is None:
             return None
         else:
             # noinspection PyProtectedMember
             raise NotImplementedError('Type Correction for %s' % t._name)
+    if input_value is None:
+        return None
     if is_dataclass(t):
         return t(**input_value)
     if issubclass(t, Enum):
@@ -113,6 +122,4 @@ def correct_type(input_value: Any, t: Type[T]) -> T:
             return t[input_value]
         except KeyError:
             return t(input_value)
-    if input_value is None:
-        return None
     return t(input_value)
