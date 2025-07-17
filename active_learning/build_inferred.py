5a15bcc55641587f749444eb34da649796a72d67
Ben Konrad Meyer <ben.meyer@bayer.com>
2025-07-17 08:21:12 +0200

    build with random bbchs

--- active_learning/build_inferred.py
+++ active_learning/build_inferred.py
@@ -1,11 +1,14 @@
+from dataclasses import replace
+import itertools
 import json
 from pathlib import Path
 
+from random import randint
 from sys import path
 path.append(str(Path(__file__).parent.parent))
 from focusStepsPelmo.ioTypes.combination import Combination
 from focusStepsPelmo.ioTypes.compound import Compound
-from focusStepsPelmo.ioTypes.gap import GAP
+from focusStepsPelmo.ioTypes.gap import GAP, RelativeGAP
 from focusStepsPelmo.ioTypes.scenario import Scenario
 from focusStepsPelmo.util.conversions import EnhancedJSONEncoder
 
@@ -15,22 +18,20 @@ compounds = Compound.from_path(path=path_to_inferred)
 print('Setup Compound Reader')
 
 path_to_gap = Path(__file__).parent.parent / 'examples' / 'gap.json'
-gap = next(GAP.from_path(path=path_to_gap))
+gap: RelativeGAP = next(GAP.from_path(path=path_to_gap)) # type: ignore
 print('Read GAP')
+gap_variations = [gap]#[replace(gap, bbch=new_bbch) for new_bbch in range(1,90,10)]
 
 combinations = []
-i = 0
 for compound in compounds:
-    i += 1
-    s_i = 0
-    for scenario in Scenario:
-        try:
-            combinations.append(Combination(compound=compound, gap=gap, scenarios=frozenset([scenario])))
-            s_i+=1
-        except ValueError:
-            pass
-    print(f"Wrote Combination number {i:5} with {s_i} scenarios")
+    for single_gap in gap_variations:
+        for scenario in Scenario:
+            mod_gap = replace(gap, bbch=randint(1,90))
+            try:
+                combinations.append(Combination(compound=compound, gap=single_gap, scenarios=frozenset([scenario])))
+            except ValueError:
+                pass
 
-out_path = Path(__file__).parent.parent / 'ppdb_combinations.json'
+out_path = Path(__file__).parent.parent / 'ppdb_combinations_with_stages.json'
 with open(out_path, 'w') as out:
     json.dump(obj=combinations, fp=out, cls=EnhancedJSONEncoder)
