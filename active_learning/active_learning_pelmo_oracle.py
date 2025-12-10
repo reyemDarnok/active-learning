6ac8decd3f381699fa007456dc85857cdda53a57
Ben Konrad Meyer <ben_konrad.meyer@smail.th-koeln.de>
2025-12-10 16:17:38 +0100

    less diagnostic spam

--- active_learning/active_learning_pelmo_oracle.py
+++ active_learning/active_learning_pelmo_oracle.py
@@ -298,10 +298,11 @@ def save_training(record: ml.TrainingRecord, save_name: str, save_dir: Path, val
     plt.savefig(save_dir / 'training_time.svg', bbox_inches='tight')
     plt.close('all')
 
+    print("saved", end=" ")
     for metric in record.scores[ml.Category.TRAIN].dataset_scores.keys():
-        print("saving", metric)
+        print(metric, end=" ")
         visualise_metric(record=record, save_dir=save_dir, metric=metric)
-    print("saved all graphics")
+    print("saved all metric graphs")
 
 def visualise_predictions(model, validation_features, validation_labels, save_dir: Path, category: ml.Category):
     plot_dir = save_dir / 'predictions' / category.value
@@ -332,9 +333,7 @@ def visualise_predictions(model, validation_features, validation_labels, save_di
 def visualise_metric(record: ml.TrainingRecord, save_dir: Path, metric: str) -> None:
     datasets = next(iter(record.scores[ml.Category.TRAIN].dataset_scores.values())).scores.keys()
     for dataset, category in product(datasets, list(ml.Category)):
-        print(dataset, ", score, ", end="")
         visualise_metric_dataset(record=record, save_dir=save_dir, metric=metric, dataset=dataset, category=category)
-        print("diagnostics, ", end="")
         visualise_model_diagnostics(record=record, save_dir=save_dir, metric=metric, dataset=dataset, category=category)
    
 
@@ -466,14 +465,10 @@ def visualise_model_diagnostics(record: ml.TrainingRecord, category: ml.Category
     scenario_scores = record.scores[category].dataset_scores[metric].scores[dataset]
     selection_path = save_dir / category.value / dataset / metric / 'diagnostics'
     selection_path.mkdir(exist_ok=True, parents=True)
-    print("std, ", end="")
     visualise_std_diagnostic(scenario_scores=scenario_scores, training_sizes=record.training_sizes, selection_path=selection_path, metric=metric, dataset=dataset, category=category)
-    print("interval, ", end="")
     visualise_interval_diagnostic(scenario_scores=scenario_scores, training_sizes=record.training_sizes, selection_path=selection_path, metric=metric, dataset=dataset, category=category)
-    print("progression, ", end="")
     visualise_progression_diagnostics(scenario_scores=scenario_scores, training_sizes=record.training_sizes, selection_path=selection_path, metric=metric, dataset=dataset, category=category)
     for slope_length in (5, 10, 20):
-        print("slope", slope_length, ", ", end="")
         visualise_slope_diagnostics(scenario_scores=scenario_scores, training_sizes=record.training_sizes, selection_path=selection_path, metric=metric, dataset=dataset, category=category, slope_length=slope_length)
 
 
